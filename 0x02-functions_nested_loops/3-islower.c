c3de5976851ceec33187ab454704ec01e1f06c41
Francoise <francoisenyiraa@gmail.com>
2022-09-13 01:57:35 -0700

    Wrote a function that checks for lowercase character.

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,15 @@
+/**
+ * _islower - This is a function that prints a string
+ * Description: This function checks /
+ * for lower case character
+ * @c: holds the value of letters
+ * Return: Returns 0
+ */
+#include "main.h"
+int _islower(int c)
+{
+	if (c >= 97 && c <= 122)
+		return (1);
+	else
+		return (0);
+}
---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+/**
+ * _isalpha - This is a function that prints a string
+ * Description: This function checks /
+ * for lower case character
+ * @c: holds the value of letters
+ * Return: Returns 0
+ */
+#include "main.h"
+
+int _isalpha(int c)
+{
+	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 132))
+		return (1);
+	else
+		return (0);
+	return (0);
+}

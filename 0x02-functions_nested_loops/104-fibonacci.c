4ba344f0c4a31364eaed065b3a775cc6cfae684d
Francoise <francoisenyiraa@gmail.com>
2022-09-13 08:03:12 -0700

    Wrote a program that finds and prints the fir st 98 Fibonacci numbers, starting with 1 and 2, followed by a new line.

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -6,27 +6,31 @@
  * Description: This program adds /                                                                                                     
  * the natural numbers up to 1024                                                                                                       
  * Return: Returns 0                                                                                                                    
- */                                                                                                                                     
-                                                                                                                                        
-int main(void)                                                                                                                          
-{                                                                                                                                       
-	unsigned long count, l, o, k;                                                                                                   
+ */
+
+int main(void)
+{
+	unsigned long count, l, o, k;
 
 
-	l = 1;                                                                                                                          
-	o = 2;                                                                                                                          
-	printf("1, 2");                                                                                                                 
-	for (count = 0 ; count < 96; count++) 
-	{                                                                                                                               
-		{                                                                                                                       
-			k = o + l;                                                                                                      
-			l = o;                                                                                                          
-			o = k;                                                                                                          
-			if (count != 96)                                                                                                
-				printf(", ");                                                                                           
-			printf("%lu", k);                                                                                               
-		}                                                                                                                       
-	}                                                                                                                               
-	printf("\n");                                                                                                                   
-	return (0);                                                                                                                     
-}
+	l = 1;
+
+	o = 2;
+
+	printf("1, 2");
+
+	for (count = 0 ; count < 48; count++)
+
+	{
+		{
+			k = o + l;
+			l = o;
+			o = k;
+			if (count != 98)
+				printf(", ");
+			printf("%lu", k);
+		}
+	}
+	printf("\n");
+	return (0);
+}	

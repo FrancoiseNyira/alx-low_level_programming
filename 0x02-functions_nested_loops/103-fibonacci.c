9c081392c93cb9f35be9016296a045be8daacfa1
Francoise <francoisenyiraa@gmail.com>
2022-09-13 07:34:40 -0700

    Wrote a program that finds and prints the sum of the even-valued terms, followed by a new line.

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main main function
+ * Descption: This program adds /
+ * the natural numbers up to 1024
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	int count, l, o, k, i;
+
+
+	l = 1;
+	o = 2;
+	i = 2;
+	for (count = 0 ; count < 32; count++)
+	{
+		{
+			k = o + l;
+			l = o;
+			o = k;
+			if (k % 2 == 0)
+				i += k;
+		}
+	}
+	printf("%d\n", i);
+	return (0);
+}

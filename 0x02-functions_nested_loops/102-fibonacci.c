b9edc56cb9213995be7508e42d284823f0876bac
Francoise <francoisenyiraa@gmail.com>
2022-09-13 07:28:59 -0700

    Printed first 50 fibonacci numbers.

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - main function
+ * Description: This program adds /
+ * the natural numbers up to 1024
+ * Return: Returns 0
+ */
+
+int main(void)
+{
+	unsigned long count, l, o, k;
+
+
+	l = 1;
+	o = 2;
+	printf("1, 2");
+	for (count = 0 ; count < 48; count++)
+	{
+		{
+			k = o + l;
+			l = o;
+			o = k;
+			if (count != 48)
+				printf(", ");
+			printf("%lu", k);
+		}
+	}
+	printf("\n");
+	return (0);
+}
